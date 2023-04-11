9e7365648230d56c095cb7d5d3956e36806402a3
Volpe6 <baasch2014@gmail.com>
2023-04-11 18:01:31 -0300

    :hammer: adjusts connections

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { useEffect, useRef, useState } from 'react'
 
 import { socket } from '../socket';
-import Peer from '@/utils/peer'
+import Peer from '@/utils/peer';
+import Video from '../components/video';
 
 
 export default function Home() {
@@ -15,11 +14,19 @@ export default function Home() {
   const remoteVideo = useRef(null);
 
   const [peerConnection, setPeerConnection] = useState(null);
-  const [peer2Connection, setPeer2Connection] = useState(null);
   const [localStream, setLocalStream] = useState(null);
-  const [polite, setPolite] = useState(null);
+  const [userName, setUserName] = useState('');
+  const [targetName, setTargetName] = useState('');
+  
+  const connections = useRef([]);
+  const refusedIce = useRef([]);
 
   useEffect(() => {
+    function findConnection(name) {
+      // return peerConnection;
+      return connections.current.find(conn => conn.target == name);
+    }
+
     function onConnect() {
       console.log('conectado ao server de sinalização');
       // alert('conectado ao server de sinalização');
@@ -30,89 +37,79 @@ export default function Home() {
       alert('desconectado do server de sinalização');
     }
 
+    function onSubscribed() {
+      alert('inscrito');
+    }
+
     function onIceCandidate(content) {
-      console.log('recebendo icecandidate');
-      if(!peerConnection) {
+      const conn = findConnection(content.name);
+      if(!conn) {
         console.log('recebeu uma icecandidato mas nao possui uma conexão rtc iniciada');
+        console.log('recusado', content);
+        socket.emit('ice-candidate-refused', {
+          name: content.target,
+          target: content.name,
+          data: content.data,
+          detail: 'nao possui uma conexão rtc iniciada'
+        });
         return;
       }
-      console.log('recebeu um candidato ices');
-      peerConnection.addIceCandidate(content.data);
+      
+      console.log('setando icecandidate');
+      conn.addIceCandidate(content.data);
     }
+    
+    // function onIceCandidateRefused(content) {
+    //   console.log(`onIceCandidateRefused ${content.name}: refuse icecandidate`, content.detail)
+    //   refusedIce.current = [...refusedIce.current, {
+    //     name: content.target,
+    //     target: content.name,
+    //     data: content.data
+    //   }];
+    // }
 
     function onNegotiation(content) {
       console.log('recebendo negociação');
-      if(!peerConnection) {
+      const conn = findConnection(content.name);
+      if(!conn) {
         console.log('recebeu uma icecandidato mas nao possui uma conexão rtc iniciada');
         return;
       }
-      peerConnection.treatNegotiation(content);
+      conn.polite = content.polite;
+      console.log(`processando negociação de: ${content.name}`);
+      conn.treatNegotiation(content);
     }
-    
-    // function onAnswer(answer) {
-    //   if(aanswer) {
-    //     console.log('foi quem criou a resposta');
-    //     return;
-    //   }
-    //   if(!peerConnection) {
-    //     console.log('recebeu uma resposta mas nao possui uma conexão rtc iniciada');
-    //     return;
-    //   }
-    //   console.log('recebeu uma resposta');
-    //   console.log('resposta', answer);
-    //   peerConnection.setRemoteDescription(answer)
-    //   .then(() => {})
-    //   .catch(error => console.log('handle aswer error:', error.toString()));
-    // }
-
-    // function onOffer(offer) {
-    //   if(origin) {
-    //     console.log('foi quem criou a oferta');
-    //     return;
-    //   }
-    //   if(!peerConnection) {
-    //     console.log('recebeu uma oferta mas nao possui uma conexão rtc iniciada');
-    //     return;
-    //   }
-    //   setAanswer(true);
-    //   console.log('recebeu uma oferta');
-    //   peerConnection.setRemoteDescription(offer)
-    //   .then(() => peerConnection.createAnswer())
-    //   .then(answer => {
-    //     peerConnection.setLocalDescription(answer);
-    //     socket.emit('answer', answer);
-    //   })
-    //   .catch((error) => console.log('handle offer error:', error.toString()));
-    // }
-
+  
     socket.on('connect', onConnect);
     socket.on('disconnect', onDisconnect);
     socket.on('negotiation', onNegotiation);
-    // socket.on('answer', onAnswer);
     socket.on('ice-candidate', onIceCandidate);
-    socket.on('hello', () => {
-      // alert('hello');
-    });
+    //o rtcpeer nao ta iniciado do outro lado
+    // socket.on('ice-candidate-refused', onIceCandidateRefused);
+    socket.on('subscribed', onSubscribed);
 
     return () => {
       socket.off('connect', onConnect);
       socket.off('disconnect', onDisconnect);
       socket.off('negotiation', onNegotiation);
-      // socket.off('answer', onAnswer);
+      socket.off('subscribed', onSubscribed);
       socket.off('ice-candidate', onIceCandidate);
+      // socket.off('ice-candidate-refused', onIceCandidateRefused);
     };
   }, [peerConnection]);
   
-  const handlePolite = (e) => {
-    const bool = e.target.checked;
-    console.log(bool);
-    setPolite(bool);
+  const subscribe = () => {
+    socket.emit('subscribe', userName);
   }
 
-  const createOffer = async () => {
-    peerConnection.createOffer();
+  const handleUserName = (event) => {
+    setUserName(event.target.value)
   }
-  
+
+  const handleTargetName = (event) => {
+    setTargetName(event.target.value)
+  }
+
   const handleVideo = () => {
     const videoTrack = localStream.getVideoTracks()[0];
     // Define a propriedade "enabled" como "false" para desativar a transmissão de vídeo
@@ -125,106 +122,95 @@ export default function Home() {
     audioTrack.enabled = !audioTrack.enabled;
   }
 
-  const start = async () => {
+  const createConnection = async () => {
+    const stream = await getMedia();
+    console.log('chamando createConnection varias vezes');
+    let peer;
     try {
-      const stream = await navigator.mediaDevices.getUserMedia({video: true, audio:true});
-      setLocalStream(stream);
-      console.log('Received local stream');
-      localVideo.current.srcObject = stream;
-      const peer1 = new Peer(true);
-      const peer2 = new Peer(false);
-      stream.getTracks().forEach(track => peer1.pc.addTrack(track, stream));
-      setPeerConnection(peer1);
-      setPeer2Connection(peer2);
-      // peer1.polite = polite;
-      peer1.attachObserver(async (content) => {
+      peer = new Peer();
+      stream.getTracks().forEach(track => peer.pc.addTrack(track, stream));
+      peer.name = userName;
+      peer.target = targetName;
+      peer.attachObserver(async (content) => {
         switch(content.type) {
+          case 'resendice':
+            // if(refusedIce.current.length>0) {
+            //   console.log('reenviadno ice');
+            //   refusedIce.current.forEach(ice => {
+            //     console.log('ice', ice);
+            //     socket.emit('ice-candidate', ice);
+            //   });
+            //   refusedIce.current = [];
+            // } else {
+            //   console.log('sem reenvio');
+            // }
+            break;
           case 'negotiation':
-            await peer2.treatNegotiation({
-              name: content.name,
-              target: content.target,
-              data: content.data
-            });
-            break;
-          // case 'negotiation':
-          //   console.log('emitindo negociação')
-          //   socket.emit('negotiation', {
-          //     name: content.name,
-          //     target: content.target,
-          //     data: content.data
-          //   });
-          //   break;
-          case 'icecandidate':
-            // console.log('emitindo icecandidate')
-            peer2.addIceCandidate(content.candidate);
-            // socket.emit('ice-candidate', {
-            //   name: content.name,
-            //   target: content.target,
-            //   data: content.candidate
-            // });
-            break;
-          case 'track':
-            console.log('lidando com track')
-            // console.log(`track`, content)
-            // const { track, streams } = content.data;
-            // remoteVideo.current.srcObject = streams[0];
-            break;
-        }
-      });
-      peer2.attachObserver(async (content) => {
-        switch(content.type) {
-          case 'negotiation':
-            await peer1.treatNegotiation({
+            console.log(content)
+            console.log(`emitindo negociação para: ${content.target}`);
+            socket.emit('negotiation', {
               name: content.name,
               target: content.target,
               data: content.data
             });
             break;
           case 'icecandidate':
-            // console.log('emitindo icecandidate')
-            peer1.addIceCandidate(content.candidate);
-            // socket.emit('ice-candidate', {
-            //   name: content.name,
-            //   target: content.target,
-            //   data: content.candidate
-            // });
+            console.log('emitindo icecandidate')
+            socket.emit('ice-candidate', {
+              name: content.name,
+              target: content.target,
+              data: content.data
+            });
             break;
           case 'track':
+            // alert('track');
             console.log('lidando com track')
             console.log(`track`, content)
-            const { track, streams } = content.data;
-            track.onunmute = () => {
-              if (remoteVideo.current.srcObject) {
-                return;
-              }
-              remoteVideo.current.srcObject = streams[0];
-            };
+            // const { track, streams } = content.data;
+            // track.onunmute = () => {
+            //   if (remoteVideo.current.srcObject) {
+            //     return;
+            //   }
+            //   remoteVideo.current.srcObject = streams[0];
+            // };
             break;
         }
       });
-      
-      
+    } catch (e) {
+      console.log(`handlePeerConnection() error: ${e.toString()}`);
+      alert(`handlePeerConnection() error: ${e.toString()}`);
+    }
+    setPeerConnection(peer);
+    connections.current = [...connections.current, peer];
+    peer.createOffer();
+  }
+
+  const getMedia = async () => {
+    let stream = null;
+    try {
+      stream = await navigator.mediaDevices.getUserMedia({video: true, audio:true});
+      setLocalStream(stream);
+      localVideo.current.srcObject = stream;
     } catch (e) {
       alert(`getUserMedia() error: ${e.toString()}`);
     }
+    return stream;
   }
 
-  const handleResize = () => {
-    console.log(`Remote video size changed to ${remoteVideo.videoWidth}x${remoteVideo.videoHeight} - Time since pageload ${performance.now().toFixed(0)}ms`);
-    // if(startTime) {
-    //   const elapsedTime = window.performance.now() - startTime;
-    //   console.log('Setup time: ' + elapsedTime.toFixed(3) + 'ms');
-    //   setStartTime(null);
-    // }
+  const call = () => {
+    // peerConnection.createOffer();
+    createConnection();
   }
 
-  const handleLoadMetadata = (e, name) => {
-    console.log(`${name} video videoWidth: ${e.target.videoWidth}px,  videoHeight: ${e.target.videoHeight}px`);
+  const call2 = () => {
+    peerConnection.createOffer();
+    // createConnection();
   }
 
- 
-  
-  
+  const restart = () => {
+    handlePeerConnection(localStream)
+  }
+
   return (
     <>
       <Head>
@@ -234,14 +220,36 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <video ref={localVideo} onLoadedMetadata={(e) => handleLoadMetadata(e,'local')} playsInline autoPlay muted></video>
-        <video ref={remoteVideo} onResize={handleResize} onLoadedMetadata={(e) => handleLoadMetadata(e, 'remote')} playsInline autoPlay></video>
+        <video ref={localVideo} playsInline autoPlay muted></video>
+        <video ref={remoteVideo} playsInline autoPlay muted></video>
+
+        <ul>
+          {connections.current.map((conn, i) => {
+            return (<li key={i}>
+              <Video peer={conn} />
+            </li>);
+          })}
+        </ul>
+
 
         <div className="box">
-            <input type="checkbox" onClick={handlePolite} />
-            <button ref={startButton} onClick={start}>start</button>
-            <button onClick={createOffer}>createOffer</button>
-            <button onClick={handleVideo}>video</button>
+          <ul>
+            <li>create connection</li>
+            <li>
+              <div className='box'>
+                <input type="text" value={userName} placeholder="username" onChange={handleUserName} />
+                <button onClick={subscribe}>subscribe</button>
+              </div>
+            </li>
+            <li>
+              <div className='box'>
+                <input type="text" value={targetName} placeholder="targetname" onChange={handleTargetName} />
+              </div>
+            </li>
+          </ul>
+          <li><button onClick={() => call()}>call</button></li>
+            
+            {/* <button onClick={handleVideo}>video</button> */}
             {/* <button ref={peerConnectionButton} onClick={handlePeerConnection}>init PeerConection</button>
             <button ref={createOfferButton} onClick={createOffer}>create offer</button>
             <button ref={hangupButton} onClick={handleHangup}>hangup</button> */}
