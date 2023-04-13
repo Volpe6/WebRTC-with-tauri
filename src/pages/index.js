23045ab4c4e6a4380b05c30367d1a08760226ffe
Rodrigo Valle <rodrigo@fitix.com.br>
2023-04-12 22:51:26 -0300

    :hammer: add hangup action again inside button click

--- src/pages/index.js
+++ src/pages/index.js
@@ -242,11 +242,131 @@ export default function Home() {
             <input type="text" value={userName} placeholder="username" onChange={handleUserName} />
             <button className='border border-fuchsia-700 bg-fuchsia-700' onClick={subscribe}>subscribe</button>
           </div>
+<<<<<<< HEAD
           <div className='flex flex-col justify-center justify-items-center space-y-5'>
             <h4>Create connection</h4>
             <input type="text" value={targetName} placeholder="targetname" onChange={handleTargetName} />
             <button className='border border-fuchsia-700 bg-fuchsia-700' onClick={call}>call</button>
             <button className='border border-fuchsia-700 bg-fuchsia-700' onClick={hangup}>desligar</button>
+=======
+          {/* Aba para apresentar se você já está inscrito ou não  */}
+          <div className="flex flex-row items-center mb-3">
+            <span className="w-3 h-3 rounded-full bg-red-500 mr-2"></span>
+            <span className="text-sm font-medium text-gray-700">You're not subscribed yet</span>
+          </div>
+          {/* Adicione novos spams abaixo caso necessário */}
+          <div className="flex flex-col">
+            <div className="flex flex-row mb-4">
+              <input
+                type="text"
+                value={userName}
+                placeholder="Enter your code"
+                onChange={handleUserName}
+                className="flex-grow mr-2 rounded-md py-2 px-3 border border-gray-400 focus:outline-none focus:border-blue-500"
+              />
+              <button
+                className="rounded-md py-2 px-4 bg-blue-500 text-white font-medium focus:outline-none hover:bg-blue-600"
+                onClick={subscribe}
+              >
+                Subscribe
+              </button>
+            </div>
+            <div className="flex flex-col mb-4">
+              <h4 className="text-sm font-medium mb-2">Create or join a meeting</h4>
+              <div className="flex flex-row mb-4">
+                <input
+                  type="text"
+                  value={targetName}
+                  placeholder="Enter your partner code"
+                  onChange={handleTargetName}
+                  className="flex-grow mr-2 rounded-md py-2 px-3 border border-gray-400 focus:outline-none focus:border-blue-500"
+                />
+                <button
+                  className="rounded-md py-2 px-4 bg-blue-500 text-white font-medium focus:outline-none hover:bg-blue-600"
+                  onClick={call}
+                >
+                  Join
+                </button>
+              </div>
+              {/* Ações possíveis de serem realizadas durante a call */}
+              {/*Estou usando os próprios ícones do tailwind mas achei o svg meio grande. Dá pra mudar futuramente */}
+              <div className="flex flex-row justify-center items-center mb-2 space-x-2">
+                <button title="Mute/Desmute Microphone" className="w-8 h-8 rounded-full bg-blue-500 text-white flex items-center justify-center">
+                  <svg viewBox="0 0 24 24" width="16" height="16" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" className="css-i6dzq1">
+                    <polyline points="15 3 21 3 21 9"></polyline>
+                    <polyline points="21 15 21 9 15 9"></polyline>
+                    <line x1="10" y1="14" x2="21" y2="3"></line>
+                    <line x1="3" y1="21" x2="21" y2="21"></line>
+                    <line x1="14" y1="10" x2="21" y2="3"></line>
+                  </svg>
+                </button>
+                <button title="Share your screen" className="w-8 h-8 rounded-full bg-green-500 text-white flex items-center justify-center">
+                  <svg viewBox="0 0 24 24" width="16" height="16" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" className="css-i6dzq1">
+                    <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+                    <line x1="9" y1="3" x2="9" y2="21"></line>
+                    <line x1="15" y1="3" x2="15" y2="21"></line>
+                    <line x1="3" y1="9" x2="21" y2="9"></line>
+                    <line x1="3" y1="15" x2="21" y2="15"></line>
+                  </svg>
+                </button>
+                <button title="Jump Out" onClick={hangup} className="w-8 h-8 rounded-full bg-red-500 text-white flex items-center justify-center">
+                  <svg viewBox="0 0 24 24" width="16" height="16" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" className="css-i6dzq1">
+                    <path d="M1 1l22 22M1 23L23 1"></path>
+                  </svg>
+
+                </button>
+              </div>
+
+            </div>
+            <div className="flex flex-col flex-grow" />
+            <h4 className="text-sm font-medium mb-2">Chat Members</h4>
+            <ul className="flex flex-col space-y-2 flex-grow overflow-auto">
+              {/* Apresentar os membros logados na sala */}
+              <li className="flex flex-row items-center">
+                <img
+                  src="https://randomuser.me/api/portraits/men/69.jpg"
+                  alt="User profile"
+                  className="w-6 h-6 rounded-full mr-2"
+                />
+                <img
+                  src="https://randomuser.me/api/portraits/women/2.jpg"
+                  alt="User profile"
+                  className="w-6 h-6 rounded-full mr-2"
+                />
+              </li>
+              {/* Componente de chat #IDEIA */}
+              <div className="absolute bottom-20 right-30 w-80 h-96 bg-white rounded-lg shadow-lg">
+                <div className="flex items-center justify-between px-7 py-3 bg-gray-200">
+                  <h4 className="font-bold text-gray-800">Chat</h4>
+                  <button className="text-gray-600  ">
+                  </button>
+                </div>
+                <div className="px-4 py-3" >
+                  <ul className="overflow-y-auto max-h-[400px]">
+                    <li className="mb-2 text-black">
+                      <span className="font-semibold">Usuário 1:</span> Olá!
+                    </li>
+                    <li className="mb-2 text-black">
+                      <span className="font-semibold">Usuário 2:</span> Oi, tudo bem?
+                    </li>
+                    <li className="mb-2 text-black">
+                      <span className="font-semibold">Usuário 1:</span> Sim
+                    </li>
+                  </ul>
+                  <div className="flex space-x-2 mt-4">
+                    <input
+                      type="text"
+                      placeholder="Digite sua mensagem"
+                      className="flex-grow border text-black border-gray-400 rounded-lg p-2"
+                    />
+                    <button className="bg-blue-500 text-black px-4 py-2 rounded-lg">
+                      Enviar
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </ul>
+>>>>>>> 1137417 (:hammer: add hangup action again inside button click)
           </div>
         </div>
         <div className='relative w-full'>
